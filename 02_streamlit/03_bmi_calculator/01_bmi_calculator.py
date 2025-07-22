34ced0f57eda4095669eeaed693bedfe844cc0f3
Muhammad Farhan Aslam <farhanmehar422538@gmail.com>
2025-07-22 15:56:06 +0500

    updated

--- 02_streamlit/03_bmi_calculator/01_bmi_calculator.py
+++ 02_streamlit/03_bmi_calculator/01_bmi_calculator.py
@@ -1,60 +1,48 @@
 # import the streamlit library
 import streamlit as st
- 
+
 # give a title to our app
 st.title('Welcome to BMI Calculator')
- 
+
 # TAKE WEIGHT INPUT in kgs
 weight = st.number_input("Enter your weight (in kgs)")
- 
+
 # TAKE HEIGHT INPUT
-# radio button to choose height format
-status = st.radio('Select your height format: ',
-                  ('cms', 'meters', 'feet'))
- 
-# compare status value
-if(status == 'cms'):
-    # take height input in centimeters
+status = st.radio('Select your height format:', ('cms', 'meters', 'feet'))
+
+height = 0
+bmi = None  # Initialize bmi outside try block
+
+if status == 'cms':
     height = st.number_input('Centimeters')
- 
-    try:
-        bmi = weight / ((height/100)**2)
-    except:
-        st.text("Enter some value of height")
- 
-elif(status == 'meters'):
-    # take height input in meters
+    if height > 0:
+        bmi = weight / ((height / 100) ** 2)
+
+elif status == 'meters':
     height = st.number_input('Meters')
- 
-    try:
+    if height > 0:
         bmi = weight / (height ** 2)
-    except:
-        st.text("Enter some value of height")
- 
+
 else:
-    # take height input in feet
     height = st.number_input('Feet')
- 
-    # 1 meter = 3.28
-    try:
-        bmi = weight / (((height/3.28))**2)
-    except:
-        st.text("Enter some value of height")
- 
-# check if the button is pressed or not
-if(st.button('Calculate BMI')):
- 
-    # print the BMI INDEX
-    st.text("Your BMI Index is {}.".format(bmi))
- 
-    # give the interpretation of BMI index
-    if(bmi < 16):
-        st.error("You are Extremely Underweight")
-    elif(bmi >= 16 and bmi < 18.5):
-        st.warning("You are Underweight")
-    elif(bmi >= 18.5 and bmi < 25):
-        st.success("Healthy")
-    elif(bmi >= 25 and bmi < 30):
-        st.warning("Overweight")
-    elif(bmi >= 30):
-        st.error("Extremely Overweight")
+    if height > 0:
+        bmi = weight / ((height / 3.28) ** 2)
+
+# Check if the button is pressed
+if st.button('Calculate BMI'):
+    if bmi is not None:
+        st.text("Your BMI Index is {:.2f}.".format(bmi))
+
+        # give the interpretation of BMI index
+        if bmi < 16:
+            st.error("You are Extremely Underweight")
+        elif 16 <= bmi < 18.5:
+            st.warning("You are Underweight")
+        elif 18.5 <= bmi < 25:
+            st.success("Healthy")
+        elif 25 <= bmi < 30:
+            st.warning("Overweight")
+        else:
+            st.error("Extremely Overweight")
+    else:
+        st.warning("Please enter valid height to calculate BMI.")
